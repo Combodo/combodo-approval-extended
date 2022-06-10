2fe8ce8853eff9f8cf01a25fde7e3b6d6bfd1a28
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-06-10 16:45:37 +0200

    :bookmark: Update module version from 1.3.3 to 1.4.0

--- module.combodo-approval-extended.php
+++ module.combodo-approval-extended.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-approval-extended/1.3.3',
+	'combodo-approval-extended/1.4.0',
 	array(
 		// Identification
 		//
